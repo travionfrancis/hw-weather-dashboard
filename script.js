0a242a73e64d4533436b2cca86c2fc3f14a62783
TraviFravi <travi1500@gmail.com>
2020-10-29 02:32:12 -0400

    Added Initial HTML placeholders and JS Pseudocode

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // Psuedocode
+
+// Get Weather Api key from OpenWeatherMap.org
+
+// Search Bar Functionality
+// Search for a City
+
+// What the search bar displays: The 5 day Forecast
+// display that data onto the dashboard html, 5 smaller containers
+ -// display City, date, Temperature, Humidity, Wind Speed, UV Index
+  -// have next upcoming dates in smaller containers
+   -// Those smaller containers will only display the date, temperature, and Humidity.
