35274b59a781cf84ffbffa574451b3372a30e7e7
Travion's Laptop <travi1500@gmail.com>
2020-11-28 14:51:15 -0800

    Added temp Local Storage Function, will change in due time

--- script.js
+++ script.js
@@ -4,7 +4,24 @@
 
 // Search Bar Functionality
 // Search for a City
+// set Local Storgae to save previous City searches
 
+
+$(document).ready(function (){
+// local storage for search
+$("#searchCityHistory").on("click", function () {
+    console.log("Pls WOrk Okay??!?");
+    var search = $(this).parent().attr("input");
+    var button = $(this).siblings(".searchButton").val();
+
+    localStorage.setItem(search, button);
+
+})
+
+
+
+
+})
 // What the search bar displays: The 5 day Forecast
 // display that data onto the dashboard html, 5 smaller containers
  -// display City, date, Temperature, Humidity, Wind Speed, UV Index
